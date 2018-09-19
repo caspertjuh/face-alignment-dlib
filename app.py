3f2dd7983a5c149858e74a4644034d333616a5c6
css <10941629+Caspertjuh@users.noreply.github.com>
2018-09-19 19:56:14 +0200

    Implementation for batch processing

--- app.py
+++ app.py
@@ -1,41 +1,61 @@
-import cv2, dlib, argparse
+import cv2, dlib, argparse, os
 from utils import extract_left_eye_center, extract_right_eye_center, get_rotation_matrix, crop_image
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Align faces in image')
     parser.add_argument('input', type=str, help='')
     parser.add_argument('output', type=str, help='')
-    parser.add_argument('--scale', metavar='S', type=int, default=4, help='an integer for the accumulator')
+    parser.add_argument('--scale', metavar='S', type=int, default=1, help='an integer for the accumulator')
     args = parser.parse_args()
 
-    input_image = args.input
-    output_image = args.output
+    input_image_folder = args.input
+    output_image_folder = args.output
     scale = args.scale
 
     detector = dlib.get_frontal_face_detector()
     predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
-    img = cv2.imread(input_image, cv2.IMREAD_GRAYSCALE)
-    height, width = img.shape[:2]
-    s_height, s_width = height // scale, width // scale
-    img = cv2.resize(img, (s_width, s_height))
+    directory = os.fsencode(input_image_folder)
 
-    dets = detector(img, 1)
+    list = os.listdir(directory)
+    maxFiles = len(list)
+    counter = 0
 
-    for i, det in enumerate(dets):
-        shape = predictor(img, det)
-        left_eye = extract_left_eye_center(shape)
-        right_eye = extract_right_eye_center(shape)
+    for file in os.listdir(directory):
+        counter = counter + 1
+        filename = os.fsdecode(file)
+        if filename.endswith(".jpg") or filename.endswith(".png") or filename.endswith(".jpeg") or filename.endswith(".bmp"):
+            print('transforming ' + str(counter) + '/' + str(maxFiles) + ': '+ filename)
+            img = cv2.imread(input_image_folder + '/' + filename, cv2.IMREAD_GRAYSCALE)
+            height, width = img.shape[:2]
+            s_height, s_width = height // scale, width // scale
+            img = cv2.resize(img, (s_width, s_height))
+            dets = detector(img, 1)
+            for i, det in enumerate(dets):
+                shape = predictor(img, det)
+                left_eye = extract_left_eye_center(shape)
+                right_eye = extract_right_eye_center(shape)
 
-        M = get_rotation_matrix(left_eye, right_eye)
-        rotated = cv2.warpAffine(img, M, (s_width, s_height), flags=cv2.INTER_CUBIC)
+                M = get_rotation_matrix(left_eye, right_eye)
+                rotated = cv2.warpAffine(img, M, (s_width, s_height), flags=cv2.INTER_CUBIC)
 
-        cropped = crop_image(rotated, det)
+                cropped = crop_image(rotated, det)
 
-        if output_image.endswith('.jpg'):
-            output_image_path = output_image.replace('.jpg', '_%i.jpg' % i)
-        elif output_image.endswith('.png'):
-            output_image_path = output_image.replace('.png', '_%i.jpg' % i)
+                if filename.endswith('.jpg'):
+                    output_image = filename.replace('.jpg', '_%i.jpg' % i)
+                elif filename.endswith('.png'):
+                    output_image = filename.replace('.png', '_%i.jpg' % i)
+                elif filename.endswith('.jpeg'):
+                    output_image = filename.replace('.jpeg', '_%i.jpg' % i)
+                elif filename.endswith('.bmp'):
+                    output_image = filename.replace('.bmp', '_%i.bmp' % i)
+                else:
+                    output_image = output_image_folder + ('_%i.jpg' % i)
+                cv2.imwrite(output_image_folder + '/' + output_image, cropped)
+
+            continue
         else:
-            output_image_path = output_image + ('_%i.jpg' % i)
-        cv2.imwrite(output_image_path, cropped)
+            continue
+
+
+
